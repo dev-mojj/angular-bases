7e092938697b89256733e86564b88645e2f6872f
Jhan Carlo <jmondragon@medalfa.mx>
2023-07-12 16:48:35 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
+
 import { CounterModule } from './counter/components/counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
+
 
 
 
@@ -16,7 +18,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
-   HeroesModule
+   HeroesModule,
+   DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
